fd8b0e5413dc8808bccc8e506cc268d6217a6879
MichaelHowe11 <michael.howell23@student.cart.org>
2024-02-14 14:37:03 -0800

    flat update

--- 1-flattening.js
+++ 1-flattening.js
@@ -2,4 +2,24 @@ let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 
 
+function reduce(array, combine, start) {
+    let current = start;
+    for (let element of array) {
+      current = combine(current, element);
+    }
+    return current;
+  }
+  
+  for (let i = 0; i < arrays.length; i++) {
+    
+  }
+  arrays = arrays.reduce((a, current) => 
+a.concat(current)
+  
+
+  );
+  
+
+  
+  console.log(arrays)
 // → [1, 2, 3, 4, 5, 6]
--- 2-your-own-loop.js
+++ 2-your-own-loop.js
@@ -1,6 +1,30 @@
-// Your code here.
+let start;
+function loop1() {
+  start = Date.now();
+  end = 100000;
+  end2 = end;
+  let numList = [];
 
-loop(3, n => n > 0, n => n - 1, console.log);
-// → 3
-// → 2
-// → 1
+  if (end >= 1) {
+
+    while (end >= 1) {
+      numList.push(end);
+      console.log(end)
+
+      end -= 1;
+    }
+
+  }
+  return (numList);
+}
+
+
+
+  function loop(num, one, two, three) {
+
+    for (one; two; index++) {
+      const element = array[index];
+
+    }
+
+  }
